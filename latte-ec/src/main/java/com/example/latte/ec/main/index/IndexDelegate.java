acf22442ac2c76397bc3b9091b11c338172363da
bunny <chendongming@rsdalstar-rd.com>
2018-03-25 15:42:02 +0800

    首页下拉刷新实现

--- latte-ec/src/main/java/com/example/latte/ec/main/index/IndexDelegate.java
+++ latte-ec/src/main/java/com/example/latte/ec/main/index/IndexDelegate.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.example.latte.delegates.bottom.BottomItemDelegate;
@@ -19,6 +20,7 @@ import com.joanzapata.iconify.widget.IconTextView;
 import java.util.WeakHashMap;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -32,7 +34,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public class IndexDelegate extends BottomItemDelegate implements View.OnFocusChangeListener {
 
-
+    private final static String TAG = IndexDelegate.class.getSimpleName();
     @BindView(R2.id.rv_index)
     RecyclerView mRecyclerView = null;
     @BindView(R2.id.srl_index)
@@ -47,6 +49,12 @@ public class IndexDelegate extends BottomItemDelegate implements View.OnFocusCha
     IconTextView mIconTextView = null;
 
     private RefreshHandler mRefreshHandler = null;
+
+    @OnClick(R2.id.icon_index_scan)
+    public void onClickScanQrCode() {
+        Log.d(TAG, "onClickScanQrCode: ");
+    }
+
 
     // TODO: 2018/3/19 测试方法，没啥卵用
     void onCallRxGet() {
@@ -110,8 +118,7 @@ public class IndexDelegate extends BottomItemDelegate implements View.OnFocusCha
                 });
     }
 
-
-    private void initRecyclerView() {
+    private void initRefreshLayout() {
         mRefreshLayout.setColorSchemeResources(
                 android.R.color.holo_blue_bright,
                 android.R.color.holo_orange_light,
@@ -120,10 +127,20 @@ public class IndexDelegate extends BottomItemDelegate implements View.OnFocusCha
         mRefreshLayout.setProgressViewOffset(true, 120, 300);
     }
 
+
+//    private void initRecyclerView() {
+//        final GridLayoutManager manager = new GridLayoutManager(getContext(), 4);
+//        mRecyclerView.setLayoutManager(manager);
+//        mRecyclerView.addItemDecoration
+//                (BaseDecoration.create(ContextCompat.getColor(getContext(), R.color.app_background), 5));
+//        final EcBottomDelegate ecBottomDelegate = getParentDelegate();
+//        mRecyclerView.addOnItemTouchListener(IndexItemClickListener.create(ecBottomDelegate));
+//    }
+
     @Override
     public void onLazyInitView(@Nullable Bundle savedInstanceState) {
         super.onLazyInitView(savedInstanceState);
-        initRecyclerView();
+        initRefreshLayout();
         mRefreshHandler.firstPage("index.php");
     }
 
@@ -135,11 +152,16 @@ public class IndexDelegate extends BottomItemDelegate implements View.OnFocusCha
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
         mRefreshHandler = new RefreshHandler(mRefreshLayout);
+
+        //onCallRxGet();
+
+        //onCallRxRestClient();
     }
 
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
 
     }
+
 
 }
--- latte-ui/src/main/java/com/example/latte_ui/refresh/RefreshHandler.java
+++ latte-ui/src/main/java/com/example/latte_ui/refresh/RefreshHandler.java
@@ -3,6 +3,7 @@ package com.example.latte_ui.refresh;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.example.latte.app.Latte;
 import com.example.latte.net.RestClient;
 import com.example.latte.net.callback.ISuccess;
@@ -11,12 +12,17 @@ import com.example.latte.net.callback.ISuccess;
  * Created by mac on 2017/10/12.
  */
 
-public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
+public class RefreshHandler implements
+        SwipeRefreshLayout.OnRefreshListener
+        , BaseQuickAdapter.RequestLoadMoreListener {
 
     private final SwipeRefreshLayout REFRESH_LAYOUT;
+//    private final PagingBean BEAN;
+//    private final RecyclerView RECYCLERVIEW;
+//    private final MultipleRecyclerAdapter mAdapter=null;
+//    private final DataConverter CONVERTER;
 
     public RefreshHandler(SwipeRefreshLayout swipeRefreshLayout) {
-
         this.REFRESH_LAYOUT = swipeRefreshLayout;
         REFRESH_LAYOUT.setOnRefreshListener(this);
     }
@@ -26,19 +32,19 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
         Latte.getHandler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                //进行一些网络请求
+                //进行一些网络请求,把下面一行代码放到网络请求回调的success中
                 REFRESH_LAYOUT.setRefreshing(false);
             }
         }, 2000);
     }
 
-    public void firstPage(String url){
+    public void firstPage(String url) {
         RestClient.builder()
                 .url(url)
                 .success(new ISuccess() {
                     @Override
                     public void onSuccess(String response) {
-                        Toast.makeText(Latte.getApplicationContext(),response,Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Latte.getApplicationContext(), response, Toast.LENGTH_SHORT).show();
                     }
                 })
                 .build()
@@ -46,8 +52,18 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
 
     }
 
+    private void paging(final String url) {
+
+    }
+
     @Override
     public void onRefresh() {
         refresh();
     }
+
+    @Override
+    public void onLoadMoreRequested() {
+        paging("refresh.php?index=");
+    }
+
 }
