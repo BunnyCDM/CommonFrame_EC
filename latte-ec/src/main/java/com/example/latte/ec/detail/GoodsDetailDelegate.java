322eaed213c112215dab4c7bea4f4fe049cc63e1
bunny <chendongming@rsdalstar-rd.com>
2018-03-29 17:12:39 +0800

    完成部分首页点击进入详情界面

--- latte-ec/src/main/java/com/example/latte/ec/detail/GoodsDetailDelegate.java
+++ latte-ec/src/main/java/com/example/latte/ec/detail/GoodsDetailDelegate.java
@@ -2,11 +2,23 @@ package com.example.latte.ec.detail;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.example.latte.delegates.LatteDelegate;
 import com.example.latte.ec.R;
+import com.example.latte.ec.R2;
+import com.example.latte_ui.animation.BezierUtil;
+import com.example.latte_ui.widget.CircleTextView;
+import com.joanzapata.iconify.widget.IconTextView;
 
+import butterknife.BindView;
 import me.yokeyword.fragmentation.anim.DefaultHorizontalAnimator;
 import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
@@ -16,37 +28,49 @@ import me.yokeyword.fragmentation.anim.FragmentAnimator;
  * 具体商品信息
  */
 
-public class GoodsDetailDelegate extends LatteDelegate {
+public class GoodsDetailDelegate extends LatteDelegate implements
+AppBarLayout.OnOffsetChangedListener,BezierUtil.AnimationListener{
+
+    // TODO: 2018/3/29 暂且记录
 
     private static final String ARG_GOODS_ID = "ARG_GOODS_ID";
 
-    /**
-     * @param goodsId
-     * @return
-     * @BindView(R2.id.app_bar_detail) AppBarLayout mAppBar = null;
-     * @BindView(R2.id.collapsing_toolbar_detail) CollapsingToolbarLayout mCollapsingToolbarLayout = null;
-     * @BindView(R2.id.detail_banner) ConvenientBanner<String> mBanner = null;
-     * //@BindView(R2.id.frame_goods_info)
-     * //ContentFrameLayout frameGoodsInfo;
-     * @BindView(R2.id.goods_detail_toolbar) Toolbar mToolbar = null;
-     * //@BindView(R2.id.icon_goods_back)
-     * //IconTextView iconGoodsBack;
-     * //@BindView(R2.id.tv_detail_title_text)
-     * //AppCompatTextView tvDetailTitleText;
-     * @BindView(R2.id.tab_layout) TabLayout mTabLayout = null;
-     * @BindView(R2.id.view_pager) ViewPager mViewPager = null;
-     * <p>
-     * //@BindView(R2.id.rl_favor)
-     * //RelativeLayout rlFavor;
-     * @BindView(R2.id.icon_favor) IconTextView mIconFavor = null;
-     * //@BindView(R2.id.ll_bottom)
-     * //LinearLayoutCompat llBottom;
-     * //@BindView(R2.id.rl_shop_cart)
-     * //RelativeLayout rlShopCart;
-     * @BindView(R2.id.icon_shop_cart) IconTextView mIconShopCart = null;
-     * @BindView(R2.id.tv_shopping_cart_amount) CircleTextView mCircleTextView = null;
-     * @BindView(R2.id.rl_add_shop_cart) RelativeLayout mRlAddShopCart = null;
-     */
+    @BindView(R2.id.app_bar_detail)
+    AppBarLayout mAppBar = null;
+    @BindView(R2.id.collapsing_toolbar_detail)
+    CollapsingToolbarLayout mCollapsingToolbarLayout = null;
+    @BindView(R2.id.detail_banner)
+    ConvenientBanner<String> mBanner = null;
+    //@BindView(R2.id.frame_goods_info)
+    //ContentFrameLayout frameGoodsInfo;
+    @BindView(R2.id.goods_detail_toolbar)
+    Toolbar mToolbar = null;
+    //@BindView(R2.id.icon_goods_back)
+    //IconTextView iconGoodsBack;
+    //@BindView(R2.id.tv_detail_title_text)
+    //AppCompatTextView tvDetailTitleText;
+    @BindView(R2.id.tab_layout)
+    TabLayout mTabLayout = null;
+
+    @BindView(R2.id.view_pager)
+    ViewPager mViewPager = null;
+
+    //@BindView(R2.id.rl_favor)
+    //RelativeLayout rlFavor;
+    @BindView(R2.id.icon_favor)
+    IconTextView mIconFavor = null;
+    //@BindView(R2.id.ll_bottom)
+    //LinearLayoutCompat llBottom;
+    //@BindView(R2.id.rl_shop_cart)
+    //RelativeLayout rlShopCart;
+    @BindView(R2.id.icon_shop_cart)
+    IconTextView mIconShopCart = null;
+    @BindView(R2.id.tv_shopping_cart_amount)
+    CircleTextView mCircleTextView = null;
+    @BindView(R2.id.rl_add_shop_cart)
+    RelativeLayout mRlAddShopCart = null;
+
+
 
 
     public static GoodsDetailDelegate create(int goodsId) {
@@ -73,4 +97,13 @@ public class GoodsDetailDelegate extends LatteDelegate {
         return new DefaultHorizontalAnimator();//返回水平打开
     }
 
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {//OnOffsetChangedListener
+
+    }
+
+    @Override
+    public void onAnimationEnd() {//AnimationListener
+
+    }
 }
